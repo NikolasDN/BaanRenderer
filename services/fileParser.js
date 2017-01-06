9fc0d4dfc0091106fc30aceca539a41faa03ed83
nikolasdn@hotmail.com <nikolasdn@hotmail.com>
2017-01-06 12:36:08 +0100

    fileparsing

--- services/fileParser.js
+++ services/fileParser.js
@@ -6,49 +6,46 @@ var fileParser = function() {
         // for(var i = 255; i < 1000; i++) {
         //     console.log(contents[i]);
         // }
-        for(var i = 0; i < contents.length; i++) {
-            var hex = this.ua2hex(contents[i]);
-            console.log(hex);
-        }
+        // for(var i = 0; i < contents.length; i++) {
+        //     var hex = this.ua2hex(contents[i]);
+        //     console.log(hex);
+        // }
         
-    //     binary.parse(contents)
-    //         .skip(256) //magic+size of header
-    //         .loop(function(endSeg, vars){
-    //             if(this.eof()){
-    //                 endSeg()
-    //             }
+        binary.parse(contents)
+            .skip(256) //magic+size of header
+            .loop(function(endSeg, vars){
+                if(this.eof()){
+                    endSeg()
+                }
 
-    //             this.word8lu('frameType')
-    //                 .word8lu('frameLen1')
-    //                 .word8lu('frameLen2')
-    //             .tap(function (vars) {
-    //             //regular segment
-    //             // if(vars.frameType == 0xFF01){
-    //             //     console.log("Found segment, length:", vars.frameLen);
-    //             //     this.skip(vars.frameLen - 4);
-    //             // } else if(vars.frameType == 0xFFFF) { //last segment
-    //             //     console.log("Done:", vars.frameLen.toString(16));
-    //             //     endSeg();
-    //             // }
-    //             if(vars.frameType == 0){
-    //                 console.log("Found segment, length:", vars.frameLen1 + vars.frameLen2);
-    //                 this.skip(vars.frameLen1 + vars.frameLen2 - 3);
-    //             }
-    //       })
-    //     })
-    //   .tap(function(vars){
-    //     console.dir(vars)
-    //   });
+                this.word8lu('frameType') // 00 = rail
+                    .word8lu('frameLen1')
+                    .word8lu('frameLen2')
+                    .word8lu('railType') // 01 is rechte
+                    .word8lu('region')
+                    .word8lu('merk') // moet 01 zijn
+                    .skip(42) // hierna begint (denk ik) de lengte
+                .tap(function (vars) {
+                //regular segment
+                // if(vars.frameType == 0xFF01){
+                //     console.log("Found segment, length:", vars.frameLen);
+                //     this.skip(vars.frameLen - 4);
+                // } else if(vars.frameType == 0xFFFF) { //last segment
+                //     console.log("Done:", vars.frameLen.toString(16));
+                //     endSeg();
+                // }
+                if(vars.frameType == 0){
+                    console.log("Found segment, length:", vars.frameLen1);
+                    this.skip(vars.frameLen1);
+                }
+          })
+        })
+      .tap(function(vars){
+        console.dir(vars)
+      });
 
     }
-
-    this.ua2hex = function(ua) {
-        var h = '';
-        for (var i = 0; i < ua.length; i++) {
-            h += "\\0x" + ua[i].toString(16);
-        }
-        return h;
-    }
+    
 };
 
 exports.fileParser = fileParser;
